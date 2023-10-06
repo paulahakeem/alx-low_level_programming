80e23ce82c7fdaf8f6086b4843ab8d615616b822
paulahakeem <paula.hakeem@yahoo.com>
2023-10-06 04:16:29 +0300

    add files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	 char *mass = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, mass, 59);
+
+	return (1);
+}
