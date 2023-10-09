d86b57ebce884647259bee3bb9c75fc6edaa0bc2
paulahakeem <paula.hakeem@yahoo.com>
2023-10-09 19:04:13 +0300

    add files

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 99)
+	{
+		digit2 = digit1;
+
+		while (digit2 <= 99)
+		{
+			if (digit1 != digit2)
+			{
+				putchar((digit1 / 10) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar(' ');
+				putchar((digit2 / 10) + '0');
+				putchar((digit2 % 10) + '0');
+
+				if (digit1 != 98 || digit2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
