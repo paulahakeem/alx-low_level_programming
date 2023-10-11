3123fb8d73d786e81cc55690b5dcd712ad406b83
paulahakeem <paula.hakeem@yahoo.com>
2023-10-11 03:52:20 +0300

    fix

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-2147483648);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
+
